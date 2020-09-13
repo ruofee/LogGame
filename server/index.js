2125314f79ac15e16a5041a52d6b89bd1536e289
陈若枫 <768047505@qq.com>
2020-09-14 00:14:32 +0800

    feat: web server add compression

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const compression = require('compression')
+
+app.use(compression())
 app.use(express.static(path.join(__dirname, '../build')))
 const port = 8991
 app.listen(port, () => {
